ff6472e94ad9f16e3364472d22196acd30d1ac8d
Ahmad Raza <mustafaraza03898@gmail.com>
2025-09-25 11:16:20 +0530

    post entity setup.

--- src/main/java/com/projects/blog_application/domain/entities/Post.java
+++ src/main/java/com/projects/blog_application/domain/entities/Post.java
@@ -1,14 +1,57 @@
 package com.projects.blog_application.domain.entities;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Post {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false ,columnDefinition = "TEXT")
+    private String content;
+
+
+    @Column(nullable = false)
+    private Integer readTime;
+
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    protected void onCreate(){
+        LocalDateTime dateTime=LocalDateTime.now();
+        this.createdAt=dateTime;
+        this.updatedAt=dateTime;
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt=LocalDateTime.now();
+    }
+
+
+
+
 }
