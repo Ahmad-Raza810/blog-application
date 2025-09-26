334fd16c403b3d250665508ef5d9799679321703
Ahmad Raza <mustafaraza03898@gmail.com>
2025-09-26 10:08:57 +0530

    returning dto

--- src/main/java/com/projects/blog_application/controllers/CategoryController.java
+++ src/main/java/com/projects/blog_application/controllers/CategoryController.java
@@ -2,23 +2,33 @@ package com.projects.blog_application.controllers;
 
 
 import com.projects.blog_application.domain.dtos.CategoryDTO;
+import com.projects.blog_application.mapper.CategoryMapper;
 import com.projects.blog_application.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/categories")
 @RequiredArgsConstructor
 public class CategoryController {
 
+    private final CategoryMapper categoryMapper;
     private  final CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<CategoryDTO> getAllCategory(){
-        return categoryService.getAllCategory();
+    public ResponseEntity<List<CategoryDTO>> getAllCategory(){
+        List<CategoryDTO> categoryDTOList=categoryService.getAllCategory()
+                .stream()
+                .map(categoryMapper::toDto).toList();
+
+
+        return new ResponseEntity<>(categoryDTOList, HttpStatus.OK);
     }
 
 
