1152be35c85942f71eeca6a1afe359b2eebc0a1f
Ahmad Raza <mustafaraza03898@gmail.com>
2025-09-28 08:21:47 +0530

    findByEmail method.

--- src/main/java/com/projects/blog_application/repositories/UserRepository.java
+++ src/main/java/com/projects/blog_application/repositories/UserRepository.java
@@ -3,7 +3,11 @@ package com.projects.blog_application.repositories;
 import com.projects.blog_application.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
 }
