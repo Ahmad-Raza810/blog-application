0e42d9b8dcab97978526e7bae9f98e65354a8c8d
Ahmad Raza <mustafaraza03898@gmail.com>
2025-09-28 08:23:00 +0530

    custom response wrapper

--- src/main/java/com/projects/blog_application/response/ApiResponse.java
+++ src/main/java/com/projects/blog_application/response/ApiResponse.java
@@ -1,4 +1,18 @@
 package com.projects.blog_application.response;
 
-public class ApiResponse {
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class ApiResponse <T>{
+    private String message;
+    private T data;
+    private int status;
+    private boolean success;
+    private LocalDateTime dateTime;
 }
