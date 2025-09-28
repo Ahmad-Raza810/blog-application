75559408ff53fb0c57526ae4d8229cf7beab83fe
Ahmad Raza <mustafaraza03898@gmail.com>
2025-09-28 08:24:01 +0530

    custom Auth-2

--- src/main/java/com/projects/blog_application/security/CustomUserDetailsService.java
+++ src/main/java/com/projects/blog_application/security/CustomUserDetailsService.java
@@ -1,4 +1,24 @@
 package com.projects.blog_application.security;
 
-public class CustomUserDetailsService {
+import com.projects.blog_application.domain.entities.User;
+import com.projects.blog_application.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user=userRepository.findByEmail(username)
+                .orElseThrow(()->new UsernameNotFoundException("user not found with email "+username));
+        return new CustomUserDetails(user);
+    }
 }
