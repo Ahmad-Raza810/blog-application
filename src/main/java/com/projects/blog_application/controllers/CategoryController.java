6ba68244bc98c641f3422b1aacf330b7c027a2f0
Ahmad Raza <mustafaraza03898@gmail.com>
2025-09-28 08:19:49 +0530

    wrapping response in custom response

--- src/main/java/com/projects/blog_application/controllers/CategoryController.java
+++ src/main/java/com/projects/blog_application/controllers/CategoryController.java
@@ -4,8 +4,8 @@ package com.projects.blog_application.controllers;
 import com.projects.blog_application.domain.dtos.CategoryDTO;
 import com.projects.blog_application.domain.dtos.CategoryRequestDTO;
 import com.projects.blog_application.domain.entities.Category;
-import com.projects.blog_application.exception.ApiErrorResponse;
 import com.projects.blog_application.mapper.CategoryMapper;
+import com.projects.blog_application.response.ApiResponse;
 import com.projects.blog_application.service.CategoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,32 +26,61 @@ public class CategoryController {
     private  final CategoryService categoryService;
 
 
+
     //endpoint for getting all categories
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> getAllCategory(){
+    public ResponseEntity<ApiResponse<List<CategoryDTO>>> getAllCategory(){
         List<CategoryDTO> categoryDTOList=categoryService.getAllCategory()
                 .stream()
                 .map(categoryMapper::toDto).toList();
 
+        ApiResponse<List<CategoryDTO>> response=new ApiResponse<List<CategoryDTO>>(
+                "Categories fetched successfully. ",
+                categoryDTOList,
+                HttpStatus.OK.value(),
+                true,
+                LocalDateTime.now()
+        );
 
-        return new ResponseEntity<>(categoryDTOList, HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 
     //endpoint for creating a category
     @PostMapping
-    public ResponseEntity<CategoryDTO> createCategory(@RequestBody @Valid CategoryRequestDTO requestDTO){
+    public ResponseEntity<ApiResponse<CategoryDTO>> createCategory(@RequestBody @Valid CategoryRequestDTO requestDTO){
         Category category=categoryMapper.toEntity(requestDTO);
         Category savedcategory=categoryService.createCategory(category);
         CategoryDTO categoryDTO=categoryMapper.toDto(savedcategory);
-        return new ResponseEntity<>(categoryDTO, HttpStatus.CREATED);
+
+        ApiResponse<CategoryDTO> response=new ApiResponse<>(
+                "category created  successfully. ",
+                categoryDTO,
+                HttpStatus.OK.value(),
+                true,
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
+
 
     //endpoint for deleting a category
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteCategory(@PathVariable("id")UUID id){
+    public ResponseEntity<ApiResponse<String>> deleteCategory(@PathVariable("id")UUID id){
         categoryService.deleteCategory(id);
-        return new ResponseEntity<>("Category deleted successfully.", HttpStatus.CREATED);
+
+        ApiResponse<String> response = ApiResponse.<String>builder()
+                .message("Category deleted successfully.")
+                .data("Category no longer exists.")
+                .status(HttpStatus.NO_CONTENT.value())
+                .success(true)
+                .dateTime(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
     }
 
 
