f66c07747f9857a2b8620728da1c30ee4eef737e
Ahmad Raza <mustafaraza03898@gmail.com>
2025-09-28 08:23:40 +0530

    custom Auth

--- src/main/java/com/projects/blog_application/security/CustomUserDetails.java
+++ src/main/java/com/projects/blog_application/security/CustomUserDetails.java
@@ -1,4 +1,33 @@
 package com.projects.blog_application.security;
 
-public class CustomUserDetails {
+import com.projects.blog_application.domain.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
 }
