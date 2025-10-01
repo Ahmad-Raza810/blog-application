e28f8feec04dccbfebb271dd96c4612a2fb94670
Ahmad Raza <mustafaraza03898@gmail.com>
2025-10-01 10:10:23 +0530

    user mapper.

--- src/main/java/com/projects/blog_application/mapper/UserMapper.java
+++ src/main/java/com/projects/blog_application/mapper/UserMapper.java
@@ -1,4 +1,16 @@
 package com.projects.blog_application.mapper;
 
+import com.projects.blog_application.domain.dtos.RegisterRequestDTO;
+import com.projects.blog_application.domain.dtos.RegisterResponseDTO;
+import com.projects.blog_application.domain.entities.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
+
+    User toEntity(RegisterRequestDTO registerRequestDTO);
+
+    RegisterResponseDTO toDto(User user);
+
 }
