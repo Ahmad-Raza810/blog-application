8eaeba867cdb68408ed082b19991331087f8281a
Ahmad Raza <mustafaraza03898@gmail.com>
2025-10-01 10:06:37 +0530

    adding validation on request dto.

--- src/main/java/com/projects/blog_application/domain/dtos/LoginRequestDTO.java
+++ src/main/java/com/projects/blog_application/domain/dtos/LoginRequestDTO.java
@@ -1,4 +1,21 @@
 package com.projects.blog_application.domain.dtos;
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequestDTO {
+
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Should be in a valid email format.")
+    private String email;
+
+    @NotBlank(message = "password is required.")
+    private String password;
 }
