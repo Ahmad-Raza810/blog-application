b858664a93f586995e176cab61ce612d5994cb90
Ahmad Raza <mustafaraza03898@gmail.com>
2025-10-01 10:03:58 +0530

    adding jwt filter before request.

--- src/main/java/com/projects/blog_application/config/SecurityConfig.java
+++ src/main/java/com/projects/blog_application/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.projects.blog_application.config;
 
 
+import com.projects.blog_application.security.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,22 +15,36 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain  securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
         http.authorizeHttpRequests(auth->auth
                         .requestMatchers(HttpMethod.GET,"/api/v1/categories").permitAll()
                         .requestMatchers(HttpMethod.POST,"/api/v1/categories").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/v1/posts").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/v1/posts").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/v1/auth/**").permitAll()
+
                 .anyRequest().authenticated()
         );
 
         //session stop
         http.sessionManagement(session->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+        // Apply JWT filter before UsernamePasswordAuthenticationFilter
+        .addFilterBefore(jwtFilter,UsernamePasswordAuthenticationFilter.class);
         return http.build();
 
     }
@@ -39,7 +56,9 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
+
 }
