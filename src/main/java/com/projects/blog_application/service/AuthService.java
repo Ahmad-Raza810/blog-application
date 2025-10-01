c1a21f769abee6ec3a67580426b7f35ec5cea974
Ahmad Raza <mustafaraza03898@gmail.com>
2025-10-01 10:13:40 +0530

    added authservice.

--- src/main/java/com/projects/blog_application/service/AuthService.java
+++ src/main/java/com/projects/blog_application/service/AuthService.java
@@ -1,4 +1,18 @@
 package com.projects.blog_application.service;
 
+import com.projects.blog_application.domain.dtos.AuthResponseDTO;
+import com.projects.blog_application.domain.dtos.LoginRequestDTO;
+import com.projects.blog_application.domain.dtos.RegisterRequestDTO;
+import com.projects.blog_application.domain.dtos.RegisterResponseDTO;
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface AuthService {
+
+
+    AuthResponseDTO login(LoginRequestDTO requestDTO);
+
+    RegisterResponseDTO register(RegisterRequestDTO registerRequestDTO);
+
+
+
 }
