5bfba7d88067dc5ab363b47ebb025992a38b8be4
Ahmad Raza <mustafaraza03898@gmail.com>
2025-10-01 10:07:52 +0530

    register response dto.

--- src/main/java/com/projects/blog_application/domain/dtos/RegisterResponseDTO.java
+++ src/main/java/com/projects/blog_application/domain/dtos/RegisterResponseDTO.java
@@ -1,4 +1,28 @@
 package com.projects.blog_application.domain.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterResponseDTO {
+
+    private UUID id;
+    private String email;
+    private String name;
+    private LocalDateTime createdAt;
+
+//    @OneToMany(orphanRemoval = true,cascade = CascadeType.ALL,mappedBy ="author")
+//    private List<Post> posts=new ArrayList<>();
+
+
+
+
+
 }
