2323c5e59fccc1b1ece2a99efe25790aca983218
Ahmad Raza <mustafaraza03898@gmail.com>
2025-10-05 10:52:11 +0530

    dto for making response a post.

--- src/main/java/com/projects/blog_application/domain/dtos/PostResponseDTO.java
+++ src/main/java/com/projects/blog_application/domain/dtos/PostResponseDTO.java
@@ -1,4 +1,28 @@
 package com.projects.blog_application.domain.dtos;
 
+import com.projects.blog_application.domain.PostStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostResponseDTO {
+
+    private UUID id;
+    private String title;
+    private String content;
+    private Integer readTime;
+    private AuthorDTO author;
+    private PostStatus postStatus;
+    private CategoryDTO category;
+    private Set<TagResponseDTO> tags;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
 }
