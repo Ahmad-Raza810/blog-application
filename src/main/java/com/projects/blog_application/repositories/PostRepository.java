61ffde60e1fcdc12f0501d577eac48d9ac56003b
Ahmad Raza <mustafaraza03898@gmail.com>
2025-10-05 10:56:00 +0530

    new methods in repositories.

--- src/main/java/com/projects/blog_application/repositories/PostRepository.java
+++ src/main/java/com/projects/blog_application/repositories/PostRepository.java
@@ -1,9 +1,18 @@
 package com.projects.blog_application.repositories;
 
+import com.projects.blog_application.domain.PostStatus;
+import com.projects.blog_application.domain.entities.Category;
 import com.projects.blog_application.domain.entities.Post;
+import com.projects.blog_application.domain.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
+
+   List<Post> findAllByPostStatusAndCategoryAndTagsContaining(PostStatus postStatus, Category category, Tag tag);
+   List<Post> findAllByPostStatusAndCategory(PostStatus postStatus, Category category);
+   List<Post> findAllByPostStatusAndTagsContaining(PostStatus postStatus, Tag tag);
+   List<Post> findAllByPostStatus(PostStatus postStatus);
 }
--- src/main/java/com/projects/blog_application/repositories/TagRepository.java
+++ src/main/java/com/projects/blog_application/repositories/TagRepository.java
@@ -2,8 +2,16 @@ package com.projects.blog_application.repositories;
 
 import com.projects.blog_application.domain.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 public interface TagRepository extends JpaRepository<Tag, UUID> {
+
+    @Query("SELECT t FROM Tag t LEFT JOIN FETCH t.posts")
+    List<Tag> findAllWithPostCount();
+
+    List<Tag> findByNameInIgnoreCase(Set<String> names);
 }
