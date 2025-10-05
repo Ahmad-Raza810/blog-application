fc81103c9e2075b58d38bb7f4f6fec9b800daefa
Ahmad Raza <mustafaraza03898@gmail.com>
2025-10-05 10:55:15 +0530

    mapper for entities.

--- src/main/java/com/projects/blog_application/mapper/PostMapper.java
+++ src/main/java/com/projects/blog_application/mapper/PostMapper.java
@@ -1,4 +1,17 @@
 package com.projects.blog_application.mapper;
 
+import com.projects.blog_application.domain.dtos.PostResponseDTO;
+import com.projects.blog_application.domain.entities.Post;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PostMapper {
+
+
+    @Mapping(target = "author",source = "author")
+    @Mapping(target = "category",source = "category")
+    @Mapping(target = "tags",source = "tags")
+    PostResponseDTO toDto(Post post);
 }
--- src/main/java/com/projects/blog_application/mapper/TagMapper.java
+++ src/main/java/com/projects/blog_application/mapper/TagMapper.java
@@ -1,4 +1,34 @@
 package com.projects.blog_application.mapper;
 
+import com.projects.blog_application.domain.PostStatus;
+import com.projects.blog_application.domain.dtos.TagRequestDTO;
+import com.projects.blog_application.domain.dtos.TagResponseDTO;
+import com.projects.blog_application.domain.entities.Post;
+import com.projects.blog_application.domain.entities.Tag;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+import java.util.Set;
+
+@Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TagMapper {
+
+    @Mapping(target = "postCount",source ="posts",qualifiedByName ="calculatePostCount")
+    TagResponseDTO toDto(Tag tag);
+
+    Tag toEntity(TagRequestDTO requestDTO);
+
+
+    @Named("calculatePostCount")
+    default  long calculatePostCount(Set<Post> posts){
+        if (posts==null)
+            return 0;
+        else
+            return  posts.stream()
+                    .filter(post-> PostStatus.PUBLISHED.equals(post.getPostStatus()))
+                    .count();
+    }
 }
