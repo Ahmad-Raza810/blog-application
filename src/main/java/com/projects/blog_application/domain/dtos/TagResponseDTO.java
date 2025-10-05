9a2206d152e632334c1ad966e9040a70045d9eb3
Ahmad Raza <mustafaraza03898@gmail.com>
2025-10-05 10:53:09 +0530

    dto for making response a tag.

--- src/main/java/com/projects/blog_application/domain/dtos/TagResponseDTO.java
+++ src/main/java/com/projects/blog_application/domain/dtos/TagResponseDTO.java
@@ -1,4 +1,17 @@
 package com.projects.blog_application.domain.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TagResponseDTO {
+    private UUID id;
+    private String name;
+    private int postCount;
 }
