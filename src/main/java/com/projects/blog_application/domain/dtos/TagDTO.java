d9145047d0a4dd3a3bdeef00624d2ae44df88196
Ahmad Raza <mustafaraza03898@gmail.com>
2025-11-18 10:21:15 +0530

    chore:a tagDto class representation of a tag in response.

--- src/main/java/com/projects/blog_application/domain/dtos/TagDTO.java
+++ src/main/java/com/projects/blog_application/domain/dtos/TagDTO.java
@@ -1,4 +1,16 @@
 package com.projects.blog_application.domain.dtos;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TagDTO {
+    private UUID id;
+    private String name;
 }
