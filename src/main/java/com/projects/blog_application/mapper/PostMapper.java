aa9ea779442e95ee9841ace7a874195ed900e2e1
Ahmad Raza <mustafaraza03898@gmail.com>
2025-11-18 10:28:59 +0530

    chore:change according to response.

--- src/main/java/com/projects/blog_application/mapper/PostMapper.java
+++ src/main/java/com/projects/blog_application/mapper/PostMapper.java
@@ -1,10 +1,16 @@
 package com.projects.blog_application.mapper;
 
 import com.projects.blog_application.domain.dtos.PostResponseDTO;
+import com.projects.blog_application.domain.dtos.TagDTO;
 import com.projects.blog_application.domain.entities.Post;
+import com.projects.blog_application.domain.entities.Tag;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PostMapper {
@@ -12,6 +18,15 @@ public interface PostMapper {
 
     @Mapping(target = "author",source = "author")
     @Mapping(target = "category",source = "category")
-    @Mapping(target = "tags",source = "tags")
+    @Mapping(target = "tags", qualifiedByName = "mapTags")
     PostResponseDTO toDto(Post post);
+
+    @Named("mapTags")
+    default Set<TagDTO> mapTags(Set<Tag> tags) {
+        if (tags == null) return null;
+
+        return tags.stream()
+                .map(tag -> new TagDTO(tag.getId(), tag.getName()))
+                .collect(Collectors.toSet());
+    }
 }
