a42391cc8cad22c97a58c9b92d810e4be84cefe5
Ahmad Raza <mustafaraza03898@gmail.com>
2025-11-18 10:34:18 +0530

    feat:implementation of user service and required method.

--- src/main/java/com/projects/blog_application/service/Impl/UserServiceImpl.java
+++ src/main/java/com/projects/blog_application/service/Impl/UserServiceImpl.java
@@ -1,4 +1,26 @@
 package com.projects.blog_application.service.Impl;
 
-public class UserServiceImpl {
+
+import com.projects.blog_application.domain.entities.User;
+import com.projects.blog_application.repositories.UserRepository;
+import com.projects.blog_application.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements  UserService{
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public User getUserById(UUID id) {
+       return userRepository.findById(id)
+               .orElseThrow(()->new EntityNotFoundException("Entity not found."));
+    }
 }
