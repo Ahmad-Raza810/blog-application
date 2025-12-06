6fffb625a0fed0efc0fc0ff7fc7fb2391dcebe4b
unknown <mustafaraza03898@gmail.com>
2025-12-06 21:46:06 +0530

    feat:a model which add admins to db.

--- backend/src/main/java/com/projects/blog_application/util/AdminLoader.java
+++ backend/src/main/java/com/projects/blog_application/util/AdminLoader.java
@@ -1,4 +1,52 @@
 package com.projects.blog_application.util;
 
-public class AdminLoader {
+import com.projects.blog_application.domain.Roles;
+import com.projects.blog_application.domain.dtos.RegisterRequestDTO;
+import com.projects.blog_application.domain.entities.User;
+import com.projects.blog_application.mapper.UserMapper;
+import com.projects.blog_application.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AdminLoader implements CommandLineRunner {
+
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    public AdminLoader(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        List<User> users = userRepository.findAll();
+
+        List<User> admins = users.stream().filter(user -> user.getUserRole() == Roles.ADMIN).toList();
+
+        if (admins.isEmpty()) {
+            User adminOne = userMapper.toEntity(new RegisterRequestDTO("admin1", "admin1@gmail.com", "1234"));
+            adminOne.setPassword(passwordEncoder.encode(adminOne.getPassword()));
+            adminOne.setUserRole(Roles.ADMIN);
+
+            User adminTwo = userMapper.toEntity(new RegisterRequestDTO("admin2", "admin2@gmail.com", "1234"));
+            adminTwo.setUserRole(Roles.ADMIN);
+            adminTwo.setPassword(passwordEncoder.encode(adminTwo.getPassword()));
+            userRepository.saveAll(List.of(adminOne, adminTwo));
+
+            System.out.println("Admin created successfully.");
+        } else {
+            System.out.println("Admin already exists.");
+        }
+
+
+    }
 }
