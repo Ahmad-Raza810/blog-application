befef172de7cbec2c7710796fd7a9b317f08a4c8
unknown <mustafaraza03898@gmail.com>
2025-12-09 21:44:25 +0530

    add refresh token entity.

--- backend/src/main/java/com/projects/blog_application/domain/entities/RefreshToken.java
+++ backend/src/main/java/com/projects/blog_application/domain/entities/RefreshToken.java
@@ -1,4 +1,39 @@
 package com.projects.blog_application.domain.entities;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true,nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expDate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id",referencedColumnName = "id")
+    private User user;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+     protected void onCreate(){
+         this.createdAt=LocalDateTime.now();
+    }
 }
