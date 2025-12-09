0012764309599a4d119f91337d94d2ecc7742bea
unknown <mustafaraza03898@gmail.com>
2025-12-09 21:54:26 +0530

    endpoint update.

--- backend/src/main/java/com/projects/blog_application/service/AuthService.java
+++ backend/src/main/java/com/projects/blog_application/service/AuthService.java
@@ -1,6 +1,6 @@
 package com.projects.blog_application.service;
 
-import com.projects.blog_application.domain.dtos.AuthResponseDTO;
+import com.projects.blog_application.domain.dtos.LoginResponseDTO;
 import com.projects.blog_application.domain.dtos.LoginRequestDTO;
 import com.projects.blog_application.domain.dtos.RegisterRequestDTO;
 import com.projects.blog_application.domain.dtos.RegisterResponseDTO;
@@ -9,7 +9,7 @@ import com.projects.blog_application.domain.dtos.RegisterResponseDTO;
 public interface AuthService {
 
 
-    AuthResponseDTO login(LoginRequestDTO requestDTO);
+    LoginResponseDTO login(LoginRequestDTO requestDTO);
 
     RegisterResponseDTO register(RegisterRequestDTO registerRequestDTO);
 
