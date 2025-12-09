0c7067824783d7dfaabbe45f5c22ca0dc7335d88
unknown <mustafaraza03898@gmail.com>
2025-12-09 21:51:21 +0530

    feat:add refresh-token endpoint.

--- backend/src/main/java/com/projects/blog_application/controllers/AuthController.java
+++ backend/src/main/java/com/projects/blog_application/controllers/AuthController.java
@@ -1,11 +1,11 @@
 package com.projects.blog_application.controllers;
 
 
-import com.projects.blog_application.domain.dtos.AuthResponseDTO;
-import com.projects.blog_application.domain.dtos.LoginRequestDTO;
-import com.projects.blog_application.domain.dtos.RegisterRequestDTO;
-import com.projects.blog_application.domain.dtos.RegisterResponseDTO;
+import com.projects.blog_application.domain.dtos.*;
+import com.projects.blog_application.domain.entities.RefreshToken;
+import com.projects.blog_application.security.JwtUtil;
 import com.projects.blog_application.service.AuthService;
+import com.projects.blog_application.service.RefreshTokenService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,12 +17,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final RefreshTokenService refreshTokenService;
     private final AuthService authService;
+    private final JwtUtil jwtUtil;
 
     //api endpoint for login user
     @PostMapping("/login")
-    public ResponseEntity<AuthResponseDTO> login(@RequestBody @Valid LoginRequestDTO requestDTO) {
-        AuthResponseDTO responseDTO = authService.login(requestDTO);
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginRequestDTO requestDTO) {
+        LoginResponseDTO responseDTO = authService.login(requestDTO);
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
 
 
@@ -33,6 +35,34 @@ public class AuthController {
     public ResponseEntity<RegisterResponseDTO> register(@RequestBody @Valid RegisterRequestDTO requestDTO) {
         RegisterResponseDTO responseDTO = authService.register(requestDTO);
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
+    }
+
+    //api endpoint for getting new Access token via refresh token
+    @PostMapping("/refresh-token")
+    public ResponseEntity<RefreshTokenResponse> getAccessToken(@RequestBody @Valid RefreshTokenRequest request) {
+
+        // 1. Validate refresh token
+        RefreshToken refreshToken = refreshTokenService.verifyRefreshToken(request.getRefreshToken());
+
+        // 2. Delete old token (rotation step)
+        refreshTokenService.deleteRefreshToken(refreshToken);
+
+        // 3. Generate new access token
+        String newAccessToken = jwtUtil.generateToken(refreshToken.getUser().getEmail());
+
+        // 4. Generate new refresh token
+        RefreshToken newRefreshToken = refreshTokenService.generateRefreshToken(refreshToken.getUser());
+
+
+        // 5. Prepare response
+        RefreshTokenResponse response = new RefreshTokenResponse(
+                newRefreshToken.getToken(),
+                newAccessToken,
+                newRefreshToken.getExpDate(),
+                jwtUtil.extractAllClaims(newAccessToken).getExpiration().toInstant()
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
