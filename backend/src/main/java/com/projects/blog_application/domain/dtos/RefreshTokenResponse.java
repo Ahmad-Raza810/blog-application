7f35abac4cd564a4de36d3f0ce4a38f7dcef7485
unknown <mustafaraza03898@gmail.com>
2025-12-09 21:54:00 +0530

    feat:introduce refreshToken response and request model.

--- backend/src/main/java/com/projects/blog_application/domain/dtos/RefreshTokenResponse.java
+++ backend/src/main/java/com/projects/blog_application/domain/dtos/RefreshTokenResponse.java
@@ -1,4 +1,19 @@
 package com.projects.blog_application.domain.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RefreshTokenResponse {
+
+    private String refreshToken;
+    private String accessToken;
+    private Instant refreshExpTime;
+    private Instant accessExpTime;
 }
