6749594ce4233896a4c63f57afa64c75474e8d81
unknown <mustafaraza03898@gmail.com>
2025-12-13 11:40:23 +0530

    add a dto for logout response.

---
+++ backend/src/main/java/com/projects/blog_application/domain/dtos/LogoutResponseDTO.java
@@ -0,0 +1,12 @@
+package com.projects.blog_application.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LogoutResponseDTO {
+    private String message;
+}
