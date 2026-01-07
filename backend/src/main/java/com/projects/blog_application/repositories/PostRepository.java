5808e0f138e4556dd6ca9fcde19ff7cd5d5a82ce
unknown <mustafaraza03898@gmail.com>
2026-01-07 21:04:23 +0530

    feat:add new repository methods for cursor based pagination an remove olds.

--- backend/src/main/java/com/projects/blog_application/repositories/PostRepository.java
+++ backend/src/main/java/com/projects/blog_application/repositories/PostRepository.java
@@ -5,20 +5,22 @@ import com.projects.blog_application.domain.entities.Category;
 import com.projects.blog_application.domain.entities.Post;
 import com.projects.blog_application.domain.entities.Tag;
 import com.projects.blog_application.domain.entities.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
 
-    List<Post> findAllByPostStatusAndCategoryAndTagsContaining(PostStatus postStatus, Category category, Tag tag);
-
-    List<Post> findAllByPostStatusAndCategory(PostStatus postStatus, Category category);
-
-    List<Post> findAllByPostStatusAndTagsContaining(PostStatus postStatus, Tag tag);
-
-    List<Post> findAllByPostStatus(PostStatus postStatus);
+//    List<Post> findAllByPostStatusAndCategoryAndTagsContaining(PostStatus postStatus, Category category, Tag tag);
+//
+//    List<Post> findAllByPostStatusAndCategory(PostStatus postStatus, Category category);
+//
+//    List<Post> findAllByPostStatusAndTagsContaining(PostStatus postStatus, Tag tag);
+//
+//    List<Post> findAllByPostStatus(PostStatus postStatus);
 
     List<Post> findAllByAuthorAndPostStatus(User user, PostStatus postStatus);
 
@@ -27,5 +29,13 @@ public interface PostRepository extends JpaRepository<Post, UUID> {
     List<Post> findTop5ByIsFeaturedTrueAndPostStatusOrderByCreatedAtDesc(PostStatus postStatus);
 
     List<Post> findTByIsTrendingTrueAndPostStatusOrderByCreatedAtDesc(PostStatus postStatus);
+
+    List<Post> findByPostStatusOrderByCreatedAtDesc(Pageable pageable,PostStatus postStatus);
+
+    List<Post> findByPostStatusAndCreatedAtLessThanOrderByCreatedAtDesc(
+            PostStatus status,
+            LocalDateTime cursor,
+            Pageable pageable
+    );
 
 }
