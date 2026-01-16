d74a9cd074930a07312fb85cd63063aa45a3ec9d
unknown <mustafaraza03898@gmail.com>
2026-01-16 16:04:20 +0530

    feat:add caffiene config.

--- backend/src/main/java/com/projects/blog_application/config/CaffeineCacheConfig.java
+++ backend/src/main/java/com/projects/blog_application/config/CaffeineCacheConfig.java
@@ -1,4 +1,25 @@
 package com.projects.blog_application.config;
 
-public class CaffieneCacheConfig {
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
+
+public class CaffeineCacheConfig {
+
+    @Bean
+    public CacheManager cacheManager(){
+        CaffeineCacheManager cacheManager=new CaffeineCacheManager();
+        cacheManager.setCaffeine(caffeine());
+        return cacheManager;
+    }
+
+    private Caffeine<Object,Object>  caffeine(){
+        return Caffeine.newBuilder()
+                .initialCapacity(100)
+                .maximumSize(500)
+                .expireAfterWrite(Duration.ofMinutes(30));
+    }
 }
